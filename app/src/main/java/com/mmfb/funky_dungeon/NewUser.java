fe3bdf3c034987db10fdac92f063ff7b97145e50
DESKTOP-HRFPAG2\natha <bergmann1@mymail.nku.edu>
2018-09-27 18:24:09 -0400

    Initial commit

---
+++ app/src/main/java/com/mmfb/funky_dungeon/NewUser.java
@@ -0,0 +1,13 @@
+package com.mmfb.funky_dungeon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+}
